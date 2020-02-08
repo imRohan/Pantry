4316eebab78428a7c9c10112fd261e2ddc60df8d
Rohan Likhite <rlikhite@gmail.com>
2020-02-08 11:35:10 -0500

    replaced redis lib, and added store()

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -10,6 +10,7 @@ class AccountController {
   public static async create(params: IAccount): Promise<IAccount> {
     try {
       const _account = new Account(params)
+      await _account.store()
       console.log('Account - Created new account!')
       return _account
     } catch(error) {
--- src/models/account.ts
+++ src/models/account.ts
@@ -1,5 +1,7 @@
 // Extarnal Libs
+import { promisify } from 'util'
 import { IsString, IsNotEmpty } from 'class-validator'
+import redis = require('redis')
 import uuidv4 = require('uuid/v4')
 
 // External Files
@@ -26,6 +28,44 @@ class Account {
   @IsString()
   public contactEmail: string
   public uuid: string
+
+  public async store(): Promise<void> {
+    try {
+      const _client = redis.createClient()
+      const _setAsync = promisify(_client.set).bind(_client)
+
+      const _accountKey = this.generateRedisKey()
+      const _stringifiedAccount = this.generateRedisPayload()
+      await _setAsync(_accountKey, _stringifiedAccount, 'EX', this.lifeSpan)
+      _client.quit()
+    } catch (error) {
+      throw new Error(`Account - failed to store account: ${error.message}`)
+    }
+  }
+
+  private readonly lifeSpan = 432000
+
+  private generateRedisPayload(): string {
+    const _accountDetails = {
+      name: this.name,
+      description: this.description,
+      contactEmail: this.contactEmail,
+      uuid: this.uuid
+    }
+    return JSON.stringify(_accountDetails)
+  }
+
+  private generateRedisKey(): string {
+    try {
+      if (!this.uuid) {
+        throw new Error('no uuid found')
+      }
+
+      return `account:${this.uuid}`
+    } catch (error) {
+      throw new Error(`Account - failed to generate rkey: ${error.message}`)
+    }
+  }
 }
 
 export = Account
