4ce6645a2bfcd1575dd4d44c1db7239fec080f61
Rohan Likhite <rlikhite@gmail.com>
2020-03-04 13:04:14 -0500

    added logic to publish all log messages to private slack

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,8 +1,11 @@
 // External Libs
 import pino = require('pino')
+import axios from 'axios'
+require('dotenv').config()
 
 class Logger {
   private logClient
+  private slackWebhook = process.env.SLACK_LOG_WEBHOOK
 
   constructor(name: string) {
     this.logClient = pino({
@@ -23,11 +26,28 @@ class Logger {
     this.sendToClient('error', message, object)
   }
 
+  // Send logs to not only the logging client, but also outbound to Slack.
   private sendToClient(level: string, message: string, object?: any) {
     if (object) {
       this.logClient[level](message, object)
     } else {
       this.logClient[level](message)
+    }
+
+    this.postToSlack(level, message)
+  }
+
+  private async postToSlack(level: string, message: string) {
+    try {
+      const _time = new Date().toLocaleTimeString()
+      const _message = `${_time} ${level.toUpperCase()} - ${message}`
+      await axios({
+        method: 'POST',
+        data: { text: _message },
+        url: this.slackWebhook,
+      })
+    } catch (error) {
+      this.logClient.error(`Could not post to Slack: ${error.message}`)
     }
   }
 }
