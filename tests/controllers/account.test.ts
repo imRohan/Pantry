cd2101d991be34af8a76fec0df399b9d511b2fa4
Rohan Likhite <contact@rohanlikhite.com>
2020-04-10 15:16:24 -0400

    added negative testing

--- tests/controllers/account.test.ts
+++ tests/controllers/account.test.ts
@@ -19,23 +19,46 @@ describe('When creating an account', () => {
     const _uuid: string = await AccountController.create(_params)
     expect(_uuid).toBeDefined()
   })
+
+  it ('throws an error if validations fail', async () => {
+    try {
+      const _params: any = {
+        description: 'Account made while testing',
+        contactEmail: 'derp@flerp.com',
+      }
+
+      await AccountController.create(_params)
+    } catch (error) {
+      expect(error.message).toMatch(/Validation failed:/)
+    }
+  })
 })
 
 describe('When retrieving an account', () => {
-  it ('returns the correct account attributes', async () => {
-    const _existingAccount: IAccountPrivate = {
-      name: 'Existing Account',
-      description: 'Account made while testing',
-      contactEmail: 'derp@flerp.com',
-      blocks: [],
-      maxNumberOfBlocks: 50,
-      notifications: true,
-      uuid: '6dc70531-d0bf-4b3a-8265-b20f8a69e180',
-    }
+  const _existingAccount: IAccountPrivate = {
+    name: 'Existing Account',
+    description: 'Account made while testing',
+    contactEmail: 'derp@flerp.com',
+    blocks: [],
+    maxNumberOfBlocks: 50,
+    notifications: true,
+    uuid: '6dc70531-d0bf-4b3a-8265-b20f8a69e180',
+  }
 
+  it ('returns the correct account attributes', async () => {
     mockedDataStore.get.mockReturnValueOnce(Promise.resolve(JSON.stringify(_existingAccount)))
 
     const _accountBase: IAccountBase = await AccountController.get(_existingAccount.uuid)
     expect(_accountBase).toBeDefined()
+  })
+
+  it ('throws an error if account does not exist', async () => {
+    try {
+      mockedDataStore.get.mockReturnValueOnce(Promise.resolve(null))
+
+      await AccountController.get(_existingAccount.uuid)
+    } catch (error) {
+      expect(error.message).toMatch(`pantry with id: ${_existingAccount.uuid} not found`)
+    }
   })
 })
