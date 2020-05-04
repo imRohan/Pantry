2a7d6350be533aa4e7755dd25e4e87299c16fc02
Rohan Likhite <contact@rohanlikhite.com>
2020-05-04 13:35:22 -0400

    added docs to config

--- src/app/config.dev.ts
+++ src/app/config.dev.ts
@@ -1,5 +1,6 @@
 const configs: any = {
   apiPath: `http://localhost:3000/apiv1`,
+  docsPath: 'https://documenter.getpostman.com/view/3281832/SzmZeMLC',
 }
 
 export = configs
--- src/app/config.prod.ts
+++ src/app/config.prod.ts
@@ -1,5 +1,6 @@
 const configs: any = {
   apiPath: 'http://getpantry.cloud/apiv1',
+  docsPath: 'https://documenter.getpostman.com/view/3281832/SzmZeMLC',
 }
 
 export = configs
