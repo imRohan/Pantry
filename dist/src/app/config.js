1d3cf1938928b8be11408defb0f0c8338c64e1a6
Rohan Likhite <rlikhite@gmail.com>
2022-03-15 11:16:53 -0400

    Remove Backend Assets from Dist Folder
    We initally added the dist folder to the repo to speed up our deploys due to the
    front end assets taking too long to compile. In doing so, we also included the
    backend assets. These items can be discarded as they do not slow down deploys.
    
    This change addresses the need by:
    * Deleting backend folders
    * Isolating the frontend assets through gitignore

--- dist/src/app/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const configs = {
-    apiPath: 'http://localhost:3000/apiv1',
-    docsPath: 'https://documenter.getpostman.com/view/3281832/SzmZeMLC',
-};
-module.exports = configs;
--- dist/src/app/config.prod.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const configs = {
-    apiPath: 'https://getpantry.cloud/apiv1',
-    docsPath: 'https://documenter.getpostman.com/view/3281832/SzmZeMLC',
-};
-module.exports = configs;
